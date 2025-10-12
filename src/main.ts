0052cda21bd0d6e1fb416b082dd08d868c6d6e25
Dylan Avila <166364578+DylanAvila04@users.noreply.github.com>
2025-10-12 22:44:04 +0000

    Step 5 complete: I have made a upgrade the user can buy. Following my theme of a bakery I made it so that when the user has 10 crossiants he can buy one chef who increases the amount of crossiants are made a second. Everytime you buy a baker the counter underneath changes +1 to show how many bakers you have. The count of how many crossiants also increases each time you buy a baker. Lastly I changed the font and color of the baker button aswell too match my cute bakery style of incremental game.

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,20 @@ import "./style.css";
 document.body.innerHTML = `
   <button> 🥐 </button>
   <div id = "Counter"> You have 0 crossiants!🥐</div>
-  <div id = "cook"> Click to hire a baker!👩‍🍳 </div>
+  <div id = "rate"> Passive income: 0.0 corssiants/sec 👩‍🍳</div>
+  <button id = "baker"> 10 crossiants = 1 Baker👩‍🍳 (Owned: 0) </button>
 `;
 
 const button = document.querySelector("button");
 const counterElement = document.getElementById("Counter");
 const rateElement = document.getElementById("rate");
+const bakerButton = document.getElementById("baker");
 // const cookElement = document.getElementById("cook");
 
 let counter = 0; // 0 crossiants to start
-const cookscrossiants = 1; // 0 crossiants from cook
+let cookscrossiants = 1; // 0 crossiants from cook
+let bakers = 0; // 0 cooks to start
+const bakerCost = 10; // cost of 1 cook
 
 // setInterval(() => {
 //   counter += cookscrossiants; // increase counter by number of cooks
@@ -27,6 +31,16 @@ const cookscrossiants = 1; // 0 crossiants from cook
 button?.addEventListener("click", () => {
   counter++; // increase counter by 1
   updateDisplay();
+});
+
+bakerButton?.addEventListener("click", () => {
+  if (counter >= bakerCost) {
+    bakers++; // increase number of cooks by 1
+    counter -= bakerCost; // decrease counter by cost of cook
+    cookscrossiants = bakers; // each cook makes 1 crossiant per second
+    updateDisplay();
+    // updateCookDisplay();
+  }
 });
 
 let pasttime: number | null = null;
@@ -56,11 +70,7 @@ function updateDisplay() {
     rateElement.textContent =
       `You are making ${cookscrossiants} crossiants per second!🥐`; // update the rate display
   }
+  if (bakerButton) {
+    bakerButton.textContent = `10 crossiants = 1 Baker👩‍🍳 (Owned: ${bakers})`; // update the baker button display
+  }
 }
-
-// function updateCookDisplay() {
-//   if (cookElement) {
-//     cookElement.textContent =
-//       `Click to hire a baker!👩‍🍳 You have ${cookscrossiants} bakers!👩‍🍳`; // update the cook display
-//   }
-// };
