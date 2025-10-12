e387463a9c6e9c5a24ff2154f80f540acc8235fa
Dylan Avila <166364578+DylanAvila04@users.noreply.github.com>
2025-10-12 22:29:40 +0000

    Step 3 complete: I have completed step 3 by adding a automatic feauture that lets the counter increase +1 automatically over time, as well when the user cliks the  🥐 button. I have an element of a baker in the site but that was to start early on the next few steps.

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,31 @@ import "./style.css";
 document.body.innerHTML = `
   <button> 🥐 </button>
   <div id = "Counter"> You have 0 crossiants!🥐</div>
+  <div id = "cook"> Click to hire a baker!👩‍🍳 </div>
 `;
+
 const button = document.querySelector("button");
 const counterElement = document.getElementById("Counter");
+// const cookElement = document.getElementById("cook");
+// const cookElement = document.getElementById("cook");
+
 let counter = 0; // 0 crossiants to start
+const cookscrossiants = 1; // 0 crossiants from cook
+
+setInterval(() => {
+  counter += cookscrossiants; // increase counter by number of cooks
+  updateDisplay();
+  // updateCookDisplay();
+}, 1000); // every second
 
 // Add an event listener to the button
 button?.addEventListener("click", () => {
   counter++; // increase counter by 1
+  updateDisplay();
+});
+
+function updateDisplay() {
   if (counterElement) {
     counterElement.textContent = `You have ${counter} crossiants!🥐`; // update the counter display
   }
-});
-//step 2 complete
+}
