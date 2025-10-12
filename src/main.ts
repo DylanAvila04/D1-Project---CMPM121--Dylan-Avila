bcd17ef678cbd73dab34d0e5af874a9f6e3f5824
Dylan Avila <166364578+DylanAvila04@users.noreply.github.com>
2025-10-12 22:35:21 +0000

    Step 4 Complete: Added a continous growth function to the counter that increases to make the count more fluid and smooth. Making it so that it grows by a fractional number.

--- src/main.ts
+++ src/main.ts
@@ -11,17 +11,17 @@ document.body.innerHTML = `
 
 const button = document.querySelector("button");
 const counterElement = document.getElementById("Counter");
-// const cookElement = document.getElementById("cook");
+const rateElement = document.getElementById("rate");
 // const cookElement = document.getElementById("cook");
 
 let counter = 0; // 0 crossiants to start
 const cookscrossiants = 1; // 0 crossiants from cook
 
-setInterval(() => {
-  counter += cookscrossiants; // increase counter by number of cooks
-  updateDisplay();
-  // updateCookDisplay();
-}, 1000); // every second
+// setInterval(() => {
+//   counter += cookscrossiants; // increase counter by number of cooks
+//   updateDisplay();
+//   // updateCookDisplay();
+// }, 1000); // every second
 
 // Add an event listener to the button
 button?.addEventListener("click", () => {
@@ -29,8 +29,38 @@ button?.addEventListener("click", () => {
   updateDisplay();
 });
 
+let pasttime: number | null = null;
+
+function loop(timestamp: number) {
+  if (!pasttime) pasttime = timestamp;
+  const diff = (timestamp - pasttime) / 1000;
+
+  counter += diff * cookscrossiants;
+  pasttime = timestamp;
+
+  updateDisplay();
+  requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
 function updateDisplay() {
   if (counterElement) {
     counterElement.textContent = `You have ${counter} crossiants!🥐`; // update the counter display
   }
+  if (counterElement) {
+    const visible = Math.floor(counter);
+    counterElement.textContent = `You have ${visible} crossiants!🥐`; // update the counter display
+  }
+  if (rateElement) {
+    rateElement.textContent =
+      `You are making ${cookscrossiants} crossiants per second!🥐`; // update the rate display
+  }
 }
+
+// function updateCookDisplay() {
+//   if (cookElement) {
+//     cookElement.textContent =
+//       `Click to hire a baker!👩‍🍳 You have ${cookscrossiants} bakers!👩‍🍳`; // update the cook display
+//   }
+// };
