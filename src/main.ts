fa816d17d6aba0bdbe7957d8581d5187e29d76b8
Dylan Avila <166364578+DylanAvila04@users.noreply.github.com>
2025-10-12 22:17:50 +0000

    Step 2 Complete: Added the clicking increase counter function to my crossiant button 🥐. Each time the user clicks the button the counter increments by +1. As well changed the font and color of the background of the site

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,16 @@ import "./style.css";
 // Create a new button
 document.body.innerHTML = `
   <button> 🥐 </button>
+  <div id = "Counter"> You have 0 crossiants!🥐</div>
 `;
+const button = document.querySelector("button");
+const counterElement = document.getElementById("Counter");
+let counter = 0; // 0 crossiants to start
+
+// Add an event listener to the button
+button?.addEventListener("click", () => {
+  counter++; // increase counter by 1
+  if (counterElement) {
+    counterElement.textContent = `You have ${counter} crossiants!🥐`; // update the counter display
+  }
+});
